92a7f2ec6f46285e9d48a1f711ca595c6abc459f
老干部 <chenjunqi@qiniu.com>
2023-11-07 10:28:20 +0800

    停止更新

--- NiuDroidPlayer/app/src/main/java/com/qiniu/droid/niuplayer/utils/UpgradeApkFileProvider.java
+++
@@ -1,6 +0,0 @@
-package com.qiniu.droid.niuplayer.utils;
-
-import android.support.v4.content.FileProvider;
-
-public class UpgradeApkFileProvider extends FileProvider {
-}
--- PLDroidPlayerDemo/app/src/main/java/com/pili/pldroid/playerdemo/utils/Config.java
+++
@@ -1,8 +0,0 @@
-package com.pili.pldroid.playerdemo.utils;
-
-import android.os.Environment;
-
-public class Config {
-    public static final String SDCARD_DIR = Environment.getExternalStorageDirectory().getAbsolutePath();
-    public static final String DEFAULT_CACHE_DIR = SDCARD_DIR + "/PLDroidPlayer";
-}
